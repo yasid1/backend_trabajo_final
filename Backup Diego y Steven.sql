a85f32b4ae9a19815c7ecf90fa152182378514f3
darroyo2 <113885194+darroyo2@users.noreply.github.com>
2022-12-01 10:13:39 -0500

    Sql de los datos Steven + Diego

---
+++ Backup Diego y Steven.sql
@@ -0,0 +1,36 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.5
+-- Dumped by pg_dump version 14.5
+
+-- Started on 2022-12-01 10:10:44
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3328 (class 0 OID 16535)
+-- Dependencies: 213
+-- Data for Name: pc_armado; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.pc_armado VALUES ('a91f1f24-52df-4540-a027-1d38761fed8e', 'Game Master', 'Lo ultimo en Gamining');
+INSERT INTO public.pc_armado VALUES ('47ad6c4a-a5e5-45a8-a3be-55f84947e8a8', 'Optimus', 'Lo mejor de lo mejor');
+
+
+-- Completed on 2022-12-01 10:10:44
+
+--
+-- PostgreSQL database dump complete
+--
+
